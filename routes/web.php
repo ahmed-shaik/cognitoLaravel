2fe0a5818d5142f919c05343de750ca03def3012
Ahmed Shaik <ahmedshaikvk18@gmail.com>
2024-06-09 07:14:52 +0530

    Added working code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    $grantCode = $request->get('code');
+    $response = Http::asForm()->withHeaders([
+        'Content-Type'=>'application/x-www-form-urlencoded',
+        'Authorization'=>'Basic NGhtYTF1Yzg0YmhsNXMzMGd1ZmJ1dXR2cnQ6M2xoNHJlMWlhZjFtOHA0cmZ2dmdsamVuN3Q1dnNlY3JxY2NsdGp0cTNlc2ttM2dvaW1q' 
+    ])->post('https://lampserver11.auth.us-west-2.amazoncognito.com/oauth2/token', [
+        'grant_type'=>'authorization_code',
+        'code'=>$grantCode,
+        'redirect_uri'=>'http://localhost:8000/'
+    ]);
+    
+    $decodedResponse = json_decode($response);
+    $accessToken = $decodedResponse->access_token;
+    $userInfoResponse = Http::asForm()->withHeaders([
+        'Content-Type'=>'application/x-www-form-urlencoded',
+        'Authorization'=>'Bearer ' .$accessToken 
+    ])->get('https://lampserver11.auth.us-west-2.amazoncognito.com/oauth2/userInfo');
+    $data = json_decode($userInfoResponse);
+
+    return view('index',['data'=>$data]);
 });
+
